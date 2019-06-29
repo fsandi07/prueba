87fe8dcd657233cf83c1bb58103496dde05ff6a1
Melvin Fallas <melfallas@yahoo.com>
2019-06-28 22:19:32 -0600

    Datasource para DotNetProject.
    Datasource para DotNetProject.

---
+++ mfallas/DotNet_TestProject/Project/DotNetProject/DotNetProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetProject.Startup))]
+namespace DotNetProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
